66285d4c8729fcce881127752165851bec74e85b
NPcarterc8 <112890610+NPcarterc8@users.noreply.github.com>
2024-03-04 10:27:58 -0500

    router and now uptodate

--- client/src/components/NavBar.vue
+++ client/src/components/NavBar.vue
@@ -1,6 +1,12 @@
 <script setup lang="ts">
 import { RouterLink } from 'vue-router';
+import { ref } from 'vue';
 
+let isActive = ref(false);
+
+function toggleMenu() {
+  isActive.value = !isActive.value;
+}
 
 </script>
 
@@ -11,16 +17,16 @@ import { RouterLink } from 'vue-router';
       <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28">
     </a>
 
-    <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+    <a role="button" @click="toggleMenu" :class="{ 'is-active': isActive } " class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
       <span aria-hidden="true"></span>
       <span aria-hidden="true"></span>
       <span aria-hidden="true"></span>
     </a>
   </div>
 
-  <div id="navbarBasicExample" class="navbar-menu">
+  <div :class="{ 'is-active': isActive } " id="navbarBasicExample" class="navbar-menu"  >
     <div class="navbar-start">
-      <RouterLink to="/" active class="navbar-item">
+      <RouterLink to="/" class="navbar-item">
         Home
       </RouterLink>
 
@@ -68,10 +74,10 @@ import { RouterLink } from 'vue-router';
 </template>
 
 <style scoped>
-.router-link-exact-active{
-    border-bottom: 2px solid ;
-}
-.router-link-active{
-    background-color: aquamarine;
-}
+
+    .router-link-active {
+        border-bottom: 2px solid #00d1b2;
+    }
+
+
 </style>
